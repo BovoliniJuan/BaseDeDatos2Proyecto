3c93f56f1b0e5643007e1afc3bfe1c03810360b8
Martina Gimenez <martigimenez2001patinadora@gmail.com>
2024-10-02 12:14:13 -0300

    Script actualizado

--- Script Tablero 1.sql
+++ Script Tablero 1.sql
@@ -1,81 +1,70 @@
-create database tablero_bda
-use tablero_bda
-
-create table provincias(
-id int primary key identity(1,1),
-nombre varchar(50))
-
-create table paises(
-id int primary key identity(1,1),
-nombre varchar(50)
-)
-
-create table localidades(
-id int primary key identity(1,1),
-nombre varchar(50),
-codigo_postal varchar(5),
-id_provincia int,
-id_pais int,
-foreign key (id_provincia) references provincias(id),
-foreign key (id_pais) references paises(id)
-)
-
-create table areas(
-id int primary key identity(1,1),
-nombre_area varchar(20),
-descripcion varchar(50)
-)
-
-create table empresas(
-id int primary key identity(1,1),
-nombre varchar(50),
-id_localidad int,
-foreign key (id_localidad) references localidades(id)
-)
-
-create table empleados(
-id int primary key identity(1,1),
-nombre varchar(20),
-apellido varchar(20),
-dni int unique
-)
-
-create table tickets(
-id int primary key identity(1,1),
-consulta varchar(250),
-id_area int,
-abierto bit,
-fecha_envio datetime,
-id_empresa int,
-foreign key (id_empresa) references empresas(id),
-foreign key (id_area) references areas(id)
-)
-
-create table tickets_respuestas(
-id_empleado int,
-id_ticket int,
-primary key(id_empleado, id_ticket),
-foreign key (id_empleado) references empleados(id),
-foreign key (id_ticket) references tickets(id),
-resuelto char,
-comentarios varchar(100),
-fecha_abierto datetime,
-fecha_cerrado datetime
-)
-
-create table areas_empresas(
-id_area int,
-id_empresa int,
-primary key(id_area,id_empresa),
-foreign key(id_area) references areas(id),
-foreign key(id_empresa) references empresas(id)
-)
-
-create table empresas_localidades(
-    id_empresa int,
-    id_localidad int,
-    direccion varchar(50),
-    foreign key (id_empleado) references empresas(id),
-    foreign key (id_localidad) references localidades(id),
-    primary key (id_empleado,id_localidad)
-)
+CREATE TABLE provincias(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50)
+);
+CREATE TABLE paises(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50)
+);
+CREATE TABLE localidades(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50),
+    codigo_postal VARCHAR(5),
+    id_provincia INT,
+    id_pais INT,
+    FOREIGN KEY(id_provincia) REFERENCES provincias(id),
+    FOREIGN KEY(id_pais) REFERENCES paises(id)
+);
+CREATE TABLE areas(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_area VARCHAR(20),
+    descripcion VARCHAR(50)
+);
+CREATE TABLE empresas(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50),
+    id_localidad INT,
+    FOREIGN KEY(id_localidad) REFERENCES localidades(id)
+);
+CREATE TABLE empleados(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(20),
+    apellido VARCHAR(20),
+    dni INT UNIQUE
+);
+CREATE TABLE tickets(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    consulta VARCHAR(250),
+    id_area INT,
+    abierto BIT,
+    fecha_envio DATETIME,
+    id_empresa INT,
+    FOREIGN KEY(id_empresa) REFERENCES empresas(id),
+    FOREIGN KEY(id_area) REFERENCES areas(id)
+);
+CREATE TABLE tickets_respuestas(
+    id_empleado INT,
+    id_ticket INT,
+    PRIMARY KEY(id_empleado, id_ticket),
+    FOREIGN KEY(id_empleado) REFERENCES empleados(id),
+    FOREIGN KEY(id_ticket) REFERENCES tickets(id),
+    resuelto CHAR,
+    comentarios VARCHAR(100),
+    fecha_abierto DATETIME,
+    fecha_cerrado DATETIME
+);
+CREATE TABLE areas_empresas(
+    id_area INT,
+    id_empresa INT,
+    PRIMARY KEY(id_area, id_empresa),
+    FOREIGN KEY(id_area) REFERENCES areas(id),
+    FOREIGN KEY(id_empresa) REFERENCES empresas(id)
+);
+CREATE TABLE empresas_localidades(
+    id_empresa INT,
+    id_localidad INT,
+    direccion VARCHAR(50),
+    FOREIGN KEY(id_empresa) REFERENCES empresas(id),
+    FOREIGN KEY(id_localidad) REFERENCES localidades(id),
+    PRIMARY KEY(id_empresa, id_localidad)
+);
