5af9dd2280138d2bb6b57f245b487a35cde4916a
Martu <martigimenez2001patinadora@gmail.com>
2024-11-12 16:58:28 -0300

    Redireccion de home a cada tablero

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,23 +1,69 @@
 // Home.js
 import React from 'react';
+import Titulo from './title';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate();
+
+  const redireccionTickets = () => {
+    navigate(`/tablaTickets`);
+  };
+  const redireccionEmpleados = () => {
+    navigate(`/tablaEmpleados`);
+  };
+  const redireccionEmpresas = () => {
+    navigate(`/tablaEmpresas`);
+  };
+
   return (
-    <div style={styles.container}>
-      <h1>Bienvenido al tablero para Tickets</h1>
-    </div>
+    <>
+      <div style={styles.body}>
+        <div style={styles.boxcontainer}>
+          <Titulo titulo="Bienvenido al tablero de Tickets" />
+          <div style={styles.container}>
+            <span style={styles.secciones} onClick={() => redireccionEmpresas()}>Empresas</span>
+            <span style={styles.secciones} onClick={() => redireccionEmpleados()}>Empleados</span>
+            <span style={styles.secciones} onClick={() => redireccionTickets()}>Tickets</span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
 const styles = {
+  body: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '100vh',
+    background: '#335c81',
+  },
   container: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    height: '100vh',
-    backgroundColor: '#f5f5f5',
   },
+  secciones: {
+    cursor: 'pointer',
+    padding: '5px',
+  },
+  boxcontainer: {
+    width: '100%',
+    maxWidth: '400px',
+    padding: '20px',
+    backgroundColor: '#f9f9f9',
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)', 
+    borderRadius: '8px', 
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: 'auto',
+    marginTop: 'auto',
+  }
 };
 
 export default Home;
